22eb5478c9577505d0d692e0d1bed9e7ace0f166
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-18 19:14:47 +0900

    use https

--- web/packages/backend/src/main.ts
+++ web/packages/backend/src/main.ts
@@ -1,10 +1,20 @@
+import { readFileSync } from 'node:fs'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
-import { BFF_PORT } from './lib/env'
+import { BFF_PORT, USE_HTTPS } from './lib/env'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const httpsOptions = USE_HTTPS
+    ? {
+        key: readFileSync(process.env.TLS_KEY_PATH || ''),
+        cert: readFileSync(process.env.TLS_CERT_PATH || ''),
+      }
+    : undefined
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  })
   app.use(cookieParser())
   await app.listen(BFF_PORT)
 }
