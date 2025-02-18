a9499cf515f3bec457ab9eab716d97f19954e002
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-19 00:07:05 +0900

    generate module

--- web/packages/backend/src/app.module.ts
+++ web/packages/backend/src/app.module.ts
@@ -1,10 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
 import { JwtService } from '@nestjs/jwt'
 import { AuthController, AuthService } from './modules/auth'
 import { HealthController, HealthService } from './modules/health'
+import { TicketModule } from './modules/ticket/ticket.module'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    TicketModule,
+  ],
   controllers: [HealthController, AuthController],
   providers: [HealthService, AuthService, JwtService],
 })
