4f963147a42c221b79342e6ef94dd387ea210532
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-20 07:54:02 +0900

    fix codegen config

--- web/packages/frontend/codegen.ts
+++ web/packages/frontend/codegen.ts
@@ -2,6 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   schema: '../backend/schema.gql',
+  documents: ['app/**/*.ts?(x)'],
   generates: {
     './app/__generated__/': {
       preset: 'client',
